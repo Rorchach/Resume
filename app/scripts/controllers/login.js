f06d57782c675af1ca0ff6eed91001a0b1da565f
Rorchach <cookmycode@gmail.com>
2014-11-26 23:22:55 +0800

    add login

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resumeApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the resumeApp
+ */
+angular.module('resumeApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.email = 'cookmycode@gmail.com';
+    $scope.password = '123456';
+
+    $scope.loginSubmit = function() {
+    	if ($scope.email === 'cookmycode@gmail.com' && $scope.password == '123456') {
+    		alert('登录成功!');
+    	} else {
+    		alert('登录失败!');
+    	}
+    }
+  });
