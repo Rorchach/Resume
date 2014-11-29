31c6000607c39f179ccf06c027ac788f48ec3b33
Rorchach <cookmycode@gmail.com>
2014-11-29 16:16:40 +0300

    simply resume

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+define(['angular', 'json!../data/user.json'], function(angular, user){
+    'use strict';
+
+    var resumeApp =  angular.module('resumeApp', [])
+                    .controller('ResumeCtrl', ['$scope', ResumeCtrl]);
+
+    function ResumeCtrl($scope) {
+        user.gender = user.gender === '0' ? '男' : '女';
+        for (var key in user.skill) {
+           user.skill[key] = user.skill[key].join('、');
+        }
+        $scope.user = user;
+    }
+
+    return resumeApp;
+});
--- app/scripts/config.js
+++ app/scripts/config.js
@@ -5,49 +5,14 @@ requirejs.config({
 
 	paths: {
 		angular: bower_c + 'angular/angular',
-		angular_resource: bower_c + 'angular-resource/angular-resource',
-		angular_route: bower_c + 'angular-route/angular-route',
+		text: bower_c + 'requirejs-text/text',
+		json: bower_c + 'requirejs-plugins/src/json',
 		app: 'app'
 	},
 
 	shim: {
-		angular: {exports: 'angular'},
-		angular_resource: {exports: 'angular_resource'},
-		angular_route: {exports: 'angular_route'}
+		angular: {exports: 'angular'}
 	}
 });
 
-define(['angular', 'angular_resource', 'angular_route'], function(angular, angular_resource, angular_route){
-  'use strict';
-
-  /**
-   * @ngdoc overview
-   * @name resumeApp
-   * @description
-   * # resumeApp
-   *
-   * Main module of the application.
-   */
-  var resumeApp =  angular
-    .module('resumeApp')
-    .config(function ($routeProvider) {
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/main.html',
-          controller: 'MainCtrl'
-        })
-        .when('/about', {
-          templateUrl: 'views/about.html',
-          controller: 'AboutCtrl'
-        })
-        .when('/login',{
-          templateUrl: 'views/login.html',
-          controller: 'LoginCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
-    });
-
-    return resumeApp;
-});
+define(['app']);
