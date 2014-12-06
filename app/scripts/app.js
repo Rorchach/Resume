cb3418c8ead612c53b0d5fd7abd833cdf69d3e76
Rorchach <cookmycode@gmail.com>
2014-12-06 22:04:35 +0800

    add sidebar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,9 +3,20 @@ define(['angular', 'ngMaterial', 'json!../data/user.json', 'ngAnimate', 'cookie'
     'use strict';
 
     var resumeApp =  angular.module('resumeApp', ['ngAnimate', 'ngCookies', 'ngMaterial'])
-                    .controller('ResumeCtrl', ['$scope', '$cookies', ResumeCtrl]);
+                    .controller('ResumeCtrl', ['$scope', '$cookies', '$timeout', '$mdSidenav', ResumeCtrl])
+                    .controller('LeftCtrl', function($scope, $timeout, $mdSidenav) {
+                      $scope.close = function() {
+                        $mdSidenav('left').close();
+                      };
+                    })
+                    .controller('RightCtrl', function($scope, $timeout, $mdSidenav) {
+                      $scope.close = function() {
+                        $mdSidenav('right').close();
+                      };
+                    });
 
-    function ResumeCtrl($scope, $cookies) {
+    function ResumeCtrl($scope, $cookies, $timeout, $mdSidenav) {
+        console.log($mdSidenav);
         user.gender = user.gender === '0' ? '男' : '女';
 
         $cookies.user = user;
@@ -15,7 +26,16 @@ define(['angular', 'ngMaterial', 'json!../data/user.json', 'ngAnimate', 'cookie'
         for (var key in user.skill) {
            user.skill[key] = user.skill[key].join('、');
         }
+
         $scope.user = user;
+
+        $scope.toggleLeft = function() {
+            console.log(11);
+            $mdSidenav('left').toggle();
+        };
+        $scope.toggleRight = function() {
+            $mdSidenav('right').toggle();
+        };
     }
 
     return resumeApp;
