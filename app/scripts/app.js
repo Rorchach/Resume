1eb31a5fc49e358a82469d53558f63c0554653e7
Rorchach <cookmycode@gmail.com>
2014-12-21 22:19:12 +0800

    clean code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,52 +1,39 @@
 define(['angular', 'ngMaterial', 'json!../data/user.json', 'UserFactory', 'ngAnimate', 'ngAria', 'ngRoute'],
     function(angular, ngMaterial, user, UserFactory){
     'use strict';
-console.log(user);
 
     angular.module('ResumeApp', [
         'ngAnimate',
         'ngMaterial',
         'ngRoute'])
         .factory('UserFactory', UserFactory)
-        .controller('ResumeCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-            $scope.user = user;
-console.log(user);
+        .controller('ResumeCtrl', function($scope, $mdSidenav, UserFactory){
+            $scope.user = UserFactory.getUser('likx');
+
             $scope.toggleLeft = function() {
                 $mdSidenav('left').toggle();
             };
-            $scope.toggleRight = function() {
-                $mdSidenav('right').toggle();
-            };
-        }])
+        })
         .controller('LeftCtrl', function($scope, $timeout, $mdSidenav) {
             $scope.close = function() {
                $mdSidenav('left').close();
             };
         })
         .controller('EditorCtrl', function($scope, $mdDialog){
-                $scope.showAlert = function(ev) {
-                    $mdDialog.show(
-                        $mdDialog.alert()
-                            .title('title')
-                            .content('content')
-                            .ok('Got it!')
-                    );
-                }
+            $scope.showAlert = function(ev) {
+                $mdDialog.show(
+                    $mdDialog.alert()
+                        .title('title')
+                        .content('content')
+                        .ok('Got it!')
+                );
+            }
 
         })
         .controller('ViewCtrl', function($scope, UserFactory){
-
-var user = UserFactory.getUser('likx')
-console.log(user);
-            for (var key in user.skill) {
-                if (user.skill[key] instanceof Array) {
-                    user.skill[key] = user.skill[key].join('、');
-                }
-            }    
-            
-            $scope.user = user;
+            $scope.user = UserFactory.getUser('likx');
         })
-        .config(function($routeProvider, $locationProvider){
+        .config(function($routeProvider){
             $routeProvider
                 .when('/editor', {
                     templateUrl: 'views/editor.html',
--- app/scripts/factories/UserFactory.js
+++ app/scripts/factories/UserFactory.js
@@ -6,6 +6,12 @@ define(['angular', 'json!../data/user.json'], function(angular, user){
 		exports.getUser = function(username) {
 			var one_user = user[username];
 			one_user.gender = one_user.gender === '0' ? '男' : '女';
+
+			for (var key in one_user.skill) {
+                if (one_user.skill[key] instanceof Array) {
+                    one_user.skill[key] = one_user.skill[key].join('、');
+                }
+            }    
 			
 			return one_user;
 		}
